0856b58796ba7f43e1e6c1773e547c81d7badf51
MyForest <david@myforest.com>
2024-04-16 01:58:50 +0000

    Restore single-threading switch workaround

--- src/solidfs.py
+++ src/solidfs.py
@@ -541,7 +541,7 @@ SolidFS enables a file system interface to a Solid Pod
 """
         + Fuse.fusage
     )
-    server = SolidFS(version="%prog " + fuse.__version__, usage=usage)
+    server = SolidFS(version="%prog " + fuse.__version__, usage=usage, dash_s_do="setsingle")
     # Set some useful defaults, they can be overridden by user-supplied command line arguments
     server.fuse_args.optdict["max_write"] = 131072
     server.fuse_args.optdict["max_read"] = 131072

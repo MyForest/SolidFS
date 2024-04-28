dceb6667b167cb656e41a545e2979c88849cf251
MyForest <david@myforest.com>
2024-04-28 23:08:31 +0000

    Raise more specific exception when unauthorized

--- src/solid_authentication.py
+++ src/solid_authentication.py
@@ -4,6 +4,7 @@ from time import time
 import requests
 import structlog
 
+from http_exception import HTTPStatusCodeException, UnauthorizedException
 from observability.tracing import Tracing
 
 
@@ -15,6 +16,8 @@ class SolidAuthentication:
         self.__common_headers = {"Session-Identifier": session_identifier, "User-Agent": "SolidFS/v0.0.1"}
 
     def authenticate_with_client_credentials(self) -> str | None:
+
+        # CSS docs: https://communitysolidserver.github.io/CommunitySolidServer/6.x/usage/client-credentials/
 
         if self.cached_token_expiry is None or time() > self.cached_token_expiry:
 
@@ -37,12 +40,15 @@ class SolidAuthentication:
             self.__logger.debug("Requesting access token", client_id=client_id, token_url=token_url, time_before_request=time_before_request)
             auth_response = requests.post(token_url, headers=headers, auth=(client_id, client_secret), data={"grant_type": "client_credentials"})
 
-            if auth_response.status_code == 200:
-                result = auth_response.json()
-                self.cached_token_expiry = time_before_request + int(result["expires_in"])
-                self.cached_token_value = result["access_token"]
-                self.__logger.debug("Generated access token", expiry=self.cached_token_expiry, token_type=result.get("token_type"), scope=result.get("scope"))
-            else:
-                raise Exception(f"Authentication failed with error {auth_response.status_code}: {auth_response.text}")
+            match auth_response.status_code:
+                case 200:
+                    result = auth_response.json()
+                    self.cached_token_expiry = time_before_request + int(result["expires_in"])
+                    self.cached_token_value = result["access_token"]
+                    self.__logger.debug("Generated access token", expiry=self.cached_token_expiry, token_type=result.get("token_type"), scope=result.get("scope"))
+                case 401:
+                    raise UnauthorizedException(auth_response.text)
+                case _:
+                    raise HTTPStatusCodeException(auth_response.status_code, auth_response.text)
 
         return self.cached_token_value
